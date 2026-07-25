f2de0096f50d870f01e6d97f240ef7913b7d6269
failedLabrat <loganaron3@gmail.com>
2026-07-25 11:19:08 +0200

    Initial commit

---
+++ helloProject/src/main.rs
@@ -0,0 +1,14 @@
+fn main (){
+    let x: i32 = -42;
+    let y: u64 = 100;
+    println!("signed integer: {}", x);
+    println!("unsigned integer: {}", y);
+}
+
+
+/*fn main() {
+    println!("Hello, Rust from cargo!");
+}
+*/
+
+//  {    }
